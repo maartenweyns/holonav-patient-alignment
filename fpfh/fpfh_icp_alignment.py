c296e49c50b4f5df4144f461cf60ff426f22e8be
Maarten Weyns <maarten.weyns@mawey.be>
2022-06-15 21:32:27 +0200

    Add downsampling to FPFH to speed up algorithm

--- fpfh/fpfh_icp_alignment.py
+++ fpfh/fpfh_icp_alignment.py
@@ -3,14 +3,27 @@ import open3d as o3d
 from fpfh.utils import fpfh_rough_registration
 from icp.p2p_icp import p2p_icp_registration
 
+
 def fpfh_icp_alignment(
         source: o3d.geometry.PointCloud,
         target_depth_sensor: o3d.geometry.PointCloud,
         target_pointer: o3d.geometry.PointCloud,
+        fpfh_downsample: bool=True,
         icp_correspondence: int=4,
         icp_max_iterations: int=30):
-    # Perform PCA rough registration
-    fpfh_rough_registration(source, target_depth_sensor)
+    """
+    Perform FPFH rough registration followed by ICP precise registration to
+    align the source point cloud to the target point clouds
+
+    :param source: Source point cloud
+    :param target_depth_sensor: Target depth sensor point cloud
+    :param target_pointer: Target pointer point cloud
+    :param fpfh_downsample: Enable or disable downsampling in the FPFH rough alignment step
+    :param icp_correspondence: Correspondence value for ICP algorithm
+    :param icp_max_iterations: Maximum allowed iterations for ICP algorithm
+    """
+    # Perform FPFH rough registration
+    fpfh_rough_registration(source, target_depth_sensor, fpfh_downsample)
     # Perform ICP precise registration
     p2p_icp_registration(source, target_depth_sensor, icp_correspondence, icp_max_iterations)
     p2p_icp_registration(source, target_pointer, icp_correspondence, icp_max_iterations)
--- fpfh/utils.py
+++ fpfh/utils.py
@@ -1,15 +1,25 @@
 import open3d as o3d
 
 
-def get_fpfh_transformation(source: o3d.geometry.PointCloud, target: o3d.geometry.PointCloud):
+def get_fpfh_transformation(
+        source: o3d.geometry.PointCloud,
+        target: o3d.geometry.PointCloud,
+        downsample: bool=True):
     """
     Get transformation matrix to roughly align source and
     target point clouds based on FPFH.
+    Downsampling can be enabled or disabled, but is enabled by default
+    to decrease alignment time
 
     :param source: The source point cloud
     :param target: The target point cloud
+    :param downsample: Enable or disable point cloud downsampling
     :return: The transformation matrix
     """
+    if downsample:
+        source = source.voxel_down_sample(2)
+        target = target.voxel_down_sample(2)
+
     source.estimate_normals(fast_normal_computation=False)
     source_fpfh = o3d.pipelines.registration.compute_fpfh_feature(
         source,
@@ -35,7 +45,10 @@ def get_fpfh_transformation(source: o3d.geometry.PointCloud, target: o3d.geometr
     return result.transformation
 
 
-def fpfh_rough_registration(source: o3d.geometry.PointCloud, target: o3d.geometry.PointCloud):
+def fpfh_rough_registration(
+        source: o3d.geometry.PointCloud,
+        target: o3d.geometry.PointCloud,
+        downsample: bool=True):
     """
     Roughly align the source point cloud to the target point cloud
     based on FPFH.
@@ -43,5 +56,5 @@ def fpfh_rough_registration(source: o3d.geometry.PointCloud, target: o3d.geometr
     :param source: The source point cloud
     :param target: The target point cloud
     """
-    transformation = get_fpfh_transformation(source, target)
+    transformation = get_fpfh_transformation(source, target, downsample)
     source.transform(transformation)
